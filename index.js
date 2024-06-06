da88e9206db0010036ee0a5115247312bbe80f02
Abdullah-FSWD <ansarimdabdullah794@gmail.com>
2024-06-06 10:24:52 +0530

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express from "express";
 import cors from "cors";
-cors.use(cors());
 const app = express();
 const port = 4000;
 
 app.use(express.json());
+app.use(cors());
 
 // Sample product data
 const products = [
